7735bfeca1374798854f5f9819bcfaea9f8491b7
Harini <harinirajahrb@gmail.com>
2025-04-02 11:10:07 +0530

    Initial commit

---
+++ src/test/java/com/jenkinsExample/JenkinsDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkinsExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
